20e8276a84a5ca73e96d667a63fd258e198f5d76
shockeyt <shockeyt@gmail.com>
2017-03-23 23:57:17 -0600

    keydown selecting finn

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 console.log("JS is working");
 
+var player1 = {
+	name: "Finn",
+	type: "human"
+};
+var player2 = {
+	name: "Jake",
+	type: "dog"
+};
+
+// function keyDown() {
+// 	player1.addEventListener("keydown", function() {
+// 		console.log("player one move");
+// 	});
+// }
+// keyDown();
+
+
 var finn = document.querySelector(".finndiv");
 
 console.log(finn);
@@ -22,3 +39,14 @@ function testJake() {
 });
 }
 testJake();
+
+
+window.addEventListener("keydown", dealWithKeyboard, false);
+// window.addEventListener("keypress", dealWithKeyboard, false);
+// window.addEventListener("keyup", dealWithKeyboard, false);
+ 
+function dealWithKeyboard(e) {
+    // gets called when any of the keyboard events are overheard
+    finn.style.border = "5px solid black";
+    console.log("kedown worked");
+}
